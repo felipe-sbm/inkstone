b0c9f283938a762b9ebccda1341248dc9fd0e3e7
felipe-sbm <129466661+felipe-sbm@users.noreply.github.com>
2026-03-24 20:26:12 -0300

    Criando um protótipo de página home:
    Eu fiquei com vontade de iniciar um protótipo da página home, e aqui está.
    
    Bem, eu acho que estou indo para um bom caminho, está ficando bem bonito, do jeito que tinha pensado.
    
    O que me orgulha é que não usei IA para desenvolver esse site.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="logo">
-      <a href="/">Inkstone <span>(烟台)</span></a>
+      <a href="/">Inkstone <span>(砚台)</span></a>
     </div>
 
     <nav>
@@ -11,7 +11,7 @@
       <a href="/about">关于该项目</a>
     </nav>
 
-    <select class="lang" id="lang" title="Select your language">
+    <select id="lang" title="Select your language">
       <option value="zh">中文</option>
       <option value="en">English</option>
       <option value="pt">Português</option>
@@ -39,7 +39,7 @@ header {
     font-size: 1.25rem;
     color: $primary-color;
     text-decoration: none;
-    
+
     span {
       font-weight: normal;
       font-size: 0.75rem;
@@ -62,15 +62,27 @@ header {
         text-decoration: dotted;
       }
 
-        &.active {
-          color: $primary-color;
-          text-decoration: underline;
-        }
+      &.active {
+        color: $primary-color;
+        text-decoration: underline;
+      }
     }
   }
 
-  .lang {
-    gap: 1rem;
+  select {
+    background-image: linear-gradient(90deg, #287af6 0%, #1d62c4 100%);
+    color: white;
+    border: none;
+    padding: 0.5rem 1rem;
+    border-radius: 0.75rem;
+    cursor: pointer;
+    font-weight: 550;
+    transition: 500ms ease;
+
+    &:focus {
+      outline: dotted 3px $tertiary-color;
+      outline-offset: 1px;
+    }
   }
 }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,229 @@
+<script setup lang="ts">
+import Yang from "@/assets/yang.webp";
+import { Star } from "@lucide/vue";
+</script>
+<!-- O melhor conversor de Hanzi para Pinyin -->
+<!-- Com o Inkstone, você pode converter facilmente caracteres chineses em pinyin. Sempre disponível e fácil de usar. -->
 <template>
-    <section class="home-page">
-        <h1>Em desenvolvimento</h1>
-        <p>Esta página está em desenvolvimento, por favor, volte mais tarde.</p>
-    </section>
+  <section class="hero">
+    <div class="introduction">
+      <h1>最好的汉字拼音转换器</h1>
+      <h2>O melhor conversor de Hanzi para Pinyin</h2>
+      <p>
+        节省您的材料准备时间。这不仅仅是一个简单的改变，而是一次精简高效的打字体验。精准、简约、优雅。
+      </p>
+      <div class="buttons">
+        <button
+          @click="$router.push('/generate')"
+          class="start"
+          title="Ir para o gerador de Pinyin"
+        >
+          Começar Agora
+        </button>
+        <button
+          :href="'https://github.com/felipe-sbm/inkstone/'"
+          target="_blank"
+          class="support"
+          title="Ir para o Github"
+        >
+          <Star :size="16" />
+          Apoiar Projeto
+        </button>
+      </div>
+    </div>
+    <div class="mascot">
+      <img :src="Yang" alt="Yang, the Inkstone mascot" />
+    </div>
+  </section>
+
+  <section class="features"></section>
+
+  <section class="bottom">
+    <div class="card">
+      <div class="top">
+        <h1>您准备好体验无比流畅的文本转换了吗？</h1>
+      </div>
+      <div class="buttons">
+        <button
+          @click="$router.push('/generate')"
+          class="start"
+          title="Ir para o gerador de Pinyin"
+        >
+          Começar Agora
+        </button>
+        <button
+          @click="$router.push('/about')"
+          class="support"
+          title="Ir para o Github"
+        >
+          Ler sobre o Projeto
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
 @use "@/style.scss" as *;
 
-.home-page {
-    padding: 1rem 0;
+.hero {
+  padding: 1rem 0;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+
+  .introduction {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5rem;
+
+    h1 {
+      font-size: 3rem;
+      color: $primary-color;
+      font-weight: 700;
+      margin-bottom: 0;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+      color: $secondary-color;
+      font-weight: 500;
+      margin: 0;
+    }
+
+    p {
+      font-size: 1.25rem;
+      color: $neutral-color;
+      font-weight: 300;
+      margin-top: 0;
+    }
+
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-content: start;
+
+      button {
+        border: none;
+        padding: 0.75rem 1.5rem;
+        font-size: 1rem;
+        border-radius: 0.75rem;
+        cursor: pointer;
+        transition: 500ms ease;
+        font-weight: 600;
+      }
+
+      .start {
+        background-image: linear-gradient(90deg, #287af6 0%, #1d62c4 100%);
+        color: white;
+
+        &:hover {
+          background-image: linear-gradient(0deg, #1d62c4 0%, #287af6 100%);
+        }
+      }
+
+      .support {
+        background-color: #dddfe2;
+        color: $neutral-color;
+        margin-left: 1rem;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+
+        &:hover {
+          background-color: #cfd0d4;
+        }
+      }
+    }
+  }
+
+  .mascot {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      max-width: 90%;
+      height: auto;
+    }
+  }
+}
+
+.bottom {
+  margin-top: 4rem;
+  display: flex;
+  justify-content: center;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(
+      180deg,
+      #002f6c 0%,
+      #01204e 80%,
+      #13294b 100%
+    );
+    padding: 2rem;
+    border-radius: 2rem;
+    max-width: 50rem;
+    width: 100%;
+    height: 15rem;
+
+    .top {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 1rem;
+      h1 {
+        color: white;
+        font-size: 2.5rem;
+        font-weight: 600;
+        margin-bottom: 1rem;
+      }
+    }
+
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      button {
+        border: none;
+        padding: 0.75rem 1.5rem;
+        font-size: 1rem;
+        border-radius: 0.75rem;
+        cursor: pointer;
+        transition: 500ms ease;
+        font-weight: 600;
+      }
+
+      .start {
+        background-color: white;
+        color: $primary-color;
+
+        &:hover {
+          background-color: #f0f0f0;
+        }
+      }
+
+      .support {
+        background-color: #dddfe27f;
+        border: 1px solid #dddfe2a9;
+        color: #f0f0f0;
+        margin-left: 1rem;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+
+        &:hover {
+          background-color: #dddfe256;
+        }
+      }
+    }
+  }
 }
 </style>
